84bf835145ff921736a145f8d81d97991041320f
Jerrod Havig <48765973+jerrodhavig@users.noreply.github.com>
2019-05-18 00:17:29 -0500

    New PHP Contact Form
    Added lots of shadows

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST["submit"])) {
+$name = $_post["name"];
+$subject = $_post["subject"];
+$mailFrom = $_post["mail"];
+$message = $_post["message"];
+
+$mailTo = "jhavig99@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: contact.php?mailsend");
+}
